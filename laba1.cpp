fcfba6fa05cca61ed4109569014ebf22d00dd410
Hodnevich <arsenius1111@gmail.com>
2026-09-07 06:31:53 +0300

    Remake main

--- laba1.cpp
+++ laba1.cpp
@@ -179,6 +179,102 @@ void printMenu(){
     cout << "0. Выход\n";
     cout << "===========================\n";
 }
+void handlePerformanceAction(Hall* selectedHall, int choice)
+{
+    if (selectedHall->getPerformanceCount() == 0)
+    {
+        cout << "В зале нет спектаклей.\n";
+        return;
+    }
+
+    selectedHall->printPerformances();
+    int index = inputInt("\nВведите номер спектакля: ");
+
+    if (index < 1 || index > selectedHall->getPerformanceCount())
+    {
+        cout << "Ошибка! Неверный номер спектакля.\n";
+        return;
+    }
+
+    index--;
+
+    if (choice == 3) // продажа билетов
+    {
+        int count = inputPositiveInt("Введите количество билетов: ");
+        selectedHall->sellTickets(index, count);
+    }
+    else if (choice == 4) // изменение данных
+    {
+        cout << "\nЧто изменить?\n";
+        cout << "1. Название\n";
+        cout << "2. Режиссёр\n";
+        cout << "3. Жанр\n";
+        cout << "4. Продолжительность\n";
+        cout << "5. Возрастное ограничение\n";
+
+        int field = inputInt("Выберите характеристику: ");
+
+        switch (field)
+        {
+        case 1:
+            selectedHall->getPerformance(index).setTitle(inputString("Новое название: "));
+            break;
+        case 2:
+            selectedHall->getPerformance(index).setDirector(inputString("Новый режиссёр: "));
+            break;
+        case 3:
+            selectedHall->getPerformance(index).setGenre(inputString("Новый жанр: "));
+            break;
+        case 4:
+            selectedHall->getPerformance(index).setDuration(inputPositiveInt("Новая продолжительность: "));
+            break;
+        case 5:
+            selectedHall->getPerformance(index).setAgeLimit(inputPositiveInt("Новое возрастное ограничение: "));
+            break;
+        default:
+            cout << "Ошибка! Неверный пункт.\n";
+        }
+    }
+    else if (choice == 5) // получить характеристику
+    {
+        cout << "\nЧто вывести?\n";
+        cout << "1. Название\n";
+        cout << "2. Режиссёр\n";
+        cout << "3. Жанр\n";
+        cout << "4. Продолжительность\n";
+        cout << "5. Возрастное ограничение\n";
+
+        int field = inputInt("Выберите характеристику: ");
+
+        switch (field)
+        {
+        case 1:
+            cout << "Название: " << selectedHall->getPerformance(index).getTitle() << endl;
+            break;
+        case 2:
+            cout << "Режиссёр: " << selectedHall->getPerformance(index).getDirector() << endl;
+            break;
+        case 3:
+            cout << "Жанр: " << selectedHall->getPerformance(index).getGenre() << endl;
+            break;
+        case 4:
+            cout << "Продолжительность: " << selectedHall->getPerformance(index).getDuration() << " мин." << endl;
+            break;
+        case 5:
+            cout << "Возрастное ограничение: " << selectedHall->getPerformance(index).getAgeLimit() << "+" << endl;
+            break;
+        default:
+            cout << "Ошибка! Неверный пункт.\n";
+        }
+    }
+    else if (choice == 6) // полная информация
+    {
+        cout << "\n===== ПОЛНАЯ ИНФОРМАЦИЯ =====\n";
+        selectedHall->getPerformance(index).printInfo();
+        cout << "Продано билетов: " << selectedHall->getSoldTickets(index) << endl;
+        cout << "Свободных мест: " << selectedHall->getCapacity() - selectedHall->getSoldTickets(index) << endl;
+    }
+}
 int main(){
     setlocale(LC_ALL, "Russian");
     Performance p1(
