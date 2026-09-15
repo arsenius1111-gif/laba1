873a04ff6a4c0e95cb8ccc9bc28653ed49b74b73
Hodnevich <arsenius1111@gmail.com>
2026-09-15 11:42:35 +0300

    Complite task

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
-#include "include/Performance.h"
-#include "include/Hall.h"
+#include "Hall.h"
+#include "Performance.h"
 #include "menu/header/MainMenu.h"
 int main() {
-    Performance p1("Гамлет", "Сергей Иванов", "Драма", 180, 16);
-    Performance p2("Ревизор", "Андрей Петров", "Комедия", 140, 12);
-    Performance p3("Щелкунчик", "Елена Смирнова", "Балет", 120, 6);
-    Hall hall1(1, 100);
-    Hall hall2(2, 250);
+    Performance p1("Гамлет", "Шекспир У.", "Трагедия", 180, 16);
+    Performance p2("Щелкунчик", "Иванов И.", "Балет", 120, 6);
+    Performance p3("Ревизор", "Гоголь Н.", "Комедия", 150, 12);
+    Hall hall1(1, 150);
+    Hall hall2(2, 80);
     hall1.addPerformance(p1);
     hall1.addPerformance(p2);
     hall2.addPerformance(p3);
