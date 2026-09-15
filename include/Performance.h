5e6ef394a56b8d090492a22109edd9241d37bbff
Hodnevich <arsenius1111@gmail.com>
2026-09-15 10:27:28 +0300

    Remake Performance.h

--- include/Performance.h
+++ include/Performance.h
@@ -1,27 +1,19 @@
 #ifndef PERFORMANCE_H
 #define PERFORMANCE_H
 #include <string>
-#include <string_view>
-using namespace std;
 class Performance {
 private:
-    string title;
-    string genre;
-    string director;
+    std::string title;
+    std::string director;
+    std::string genre;
     int duration;
-    int ageLimit;
+    int ageRestriction;
 public:
-    Performance(string_view title_, string_view director_, string_view genre_, int duration_, int ageLimit_);
-    void setTitle(string_view title_);
-    void setDirector(string_view director_);
-    void setGenre(string_view genre_);
-    void setDuration(int duration_);
-    void setAgeLimit(int ageLimit_);
-    string getTitle() const;
-    string getDirector() const;
-    string getGenre() const;
+    Performance(std::string title_, std::string director_, std::string genre_, int duration_, int ageRestriction_);
+    std::string getTitle() const;
+    std::string getDirector() const;
+    std::string getGenre() const;
     int getDuration() const;
-    int getAgeLimit() const;
-    void printInfo() const;
+    int getAgeRestriction() const;
 };
-#endif
+#endif 
